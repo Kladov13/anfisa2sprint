4434932fe8fb4ee155c91a7b6c70b7c52e6934c4
Kladov13 <kladov113@yandex.com>
2024-06-21 10:25:57 +0300

    Final ice cream

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+from django.db.models import Q
+
+from ice_cream.models import IceCream
+
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.values(
+         'id', 'title', 'price', 'description'
+    ).filter(
+        is_published=True,  # Сорт разрешён к публикации;
+        is_on_main=True,  # Сорт разрешён к публикации на главной странице;
+        category__is_published=True
+    ).order_by('title')[1:4]     
+
+    
+    context = {
+        'ice_cream_list': ice_cream_list,       
+    }
+    
+    return render(request, template, context)
